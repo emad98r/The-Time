4a65d893d77479a3d496dfe74f2c61f9f6b7fcaa
Emad Rashad <emad98rashad@gmail.com>
2022-02-19 07:05:52 +0530

    fix the first section slider

--- main.js
+++ main.js
@@ -52,165 +52,45 @@ shareBut.addEventListener("click",()=>{
 // next and previous buttons---------------
 let leftArr =document.querySelector('#leftArr'),
 rightArr =document.querySelector('#rightArr'),
-bigTitle =document.querySelector('#bigTitle'),
-smallTitle =document.querySelector('#smallTitle'),
-paragraph =document.querySelector('#paragraph'),
-bigSection =document.querySelector('#bigSection'),
+slides =document.querySelectorAll('#bigSection'),
 numbers =document.querySelector('#numbers');
 
+current = 0;
+function setDefault(){
+  slides[current].style.display="block"
+  numbers.innerHTML=`01`
 
-
-
-
-function sectionNo1(){
-  smallTitle.innerHTML='Brands'
-  bigTitle.innerHTML="time tag watch"
-  paragraph.innerHTML="Sometimes, we need to check the time, wondering when our work or meeting will finish, without getting caught by others."
-  numbers.innerHTML="01"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 4.jpg");`
 }
-function sectionNo2(){
-  bigTitle.innerHTML="under armour"
-  paragraph.innerHTML="Cal was first. The first public university in the great state of California. They are the pioneers. They are the trailblazers who started it all."
-  numbers.innerHTML="02"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 1.jpg");`
-}
-function sectionNo3(){
-  smallTitle.innerHTML='PHOTOGRAPHY'
-  bigTitle.innerHTML="re styling"
-  paragraph.innerHTML="Once the brand strategy was sharp and real for everyone inside of the company, all the brand behavior started to roll out as stationary material."
-  numbers.innerHTML="03"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 2.jpg");`
-}
-function sectionNo4(){
-  smallTitle.innerHTML='PHOTOGRAPHY'
-  bigTitle.innerHTML="toast 2022 reel"
-  paragraph.innerHTML="We are thrilled to share our new reel with you all! Special thanks to all of our talented friends."
-  numbers.innerHTML="04"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 3.jpg");`
-}
-function sectionNo5(){
-  smallTitle.innerHTML='PHOTOGRAPHY'
-  bigTitle.innerHTML="nile - kabutha"
-  paragraph.innerHTML="We are thrilled to share our new reel with you all! Special thanks to all of our talented friends."
-  numbers.innerHTML="05"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 5.jpg");`
-}
-function sectionNo6(){
-  smallTitle.innerHTML='PHOTOGRAPHY'
-  bigTitle.innerHTML="sleep walker"
-  paragraph.innerHTML=""
-  numbers.innerHTML="06"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 6.jpg");`
-}
-function sectionNo7(){
-  smallTitle.innerHTML='SPORTS'
-  bigTitle.innerHTML="magista"
-  paragraph.innerHTML="The Brief team has been sincerely committed to designing great communication around our projects."
-  numbers.innerHTML="07"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 7.jpg");`
-}
-function sectionNo8(){
-  smallTitle.innerHTML='PHOTOGRAPHY'
-  bigTitle.innerHTML="bastian bux"
-  paragraph.innerHTML="Bastian bux is the consequence of reducing everything surrounding a dj and producer to its essential element: the music."
-  numbers.innerHTML="08"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 8.jpg");`
-}
-function sectionNo9(){
-  smallTitle.innerHTML='ARCHITECTURE'
-  bigTitle.innerHTML="novara conic"
-  paragraph.innerHTML="Cal was first. The first public university in the great state of California. They are the pioneers."
-  numbers.innerHTML="09"
-  bigSection.style.cssText=`background-image: url("../../imgs/section\ 1\ 9.jpg");`
-}
+setDefault()
 
 rightArr.addEventListener("click",()=>{
-  if(numbers.innerHTML='01'){
-    sectionNo2()
+  slides[current].style.display="none";
+  slides[current].classList.remove("animated")
+
+  current=current+1;
+  if(current==slides.length){
+    current=0;
   }
-  rightArr.addEventListener("click",()=>{
-    if(numbers.innerHTML='02'){
-      sectionNo3()
-    }
-    rightArr.addEventListener("click",()=>{
-      if(numbers.innerHTML='03'){
-        sectionNo4()
-      }
-      rightArr.addEventListener("click",()=>{
-        if(numbers.innerHTML='04'){
-          sectionNo5()
-        }
-        rightArr.addEventListener("click",()=>{
-          if(numbers.innerHTML='05'){
-            sectionNo6()
-          }
-          rightArr.addEventListener("click",()=>{
-            if(numbers.innerHTML='06'){
-              sectionNo7()
-            }
-            rightArr.addEventListener("click",()=>{
-              if(numbers.innerHTML='07'){
-                sectionNo8()
-              }
-              rightArr.addEventListener("click",()=>{
-                if(numbers.innerHTML='08'){
-                  sectionNo9()
-                }
-              })
-            })
-          })
-        })
-      })
-    })
-  })
+  slides[current].style.display="block"
+  numbers.innerHTML=`0${current+1}`
+  slides[current].classList.add("animated")
 })
 
+leftArr.addEventListener("click",()=>{
+  slides[current].style.display="none";
+  slides[current].classList.remove("animated")
+  numbers.innerHTML=`0${current}`
+  current=current-1;
+  if(current<0){
+    current=slides.length-1;
+    numbers.innerHTML=`0${current+1}`
+  }
+  slides[current].style.display="block"
+  slides[current].classList.add("animated")
 
 
-// --------------
+})
 
-
-
-
-leftArr.addEventListener("click",sectionNo1)
-  
-//   leftArr.addEventListener("click",()=>{
-//     if(numbers.innerHTML='03'){
-//       sectionNo2()
-//     }
-//     leftArr.addEventListener("click",()=>{
-//       if(numbers.innerHTML='04'){
-//         sectionNo3()
-//       }
-//       leftArr.addEventListener("click",()=>{
-//         if(numbers.innerHTML='05'){
-//           sectionNo4()
-//         }
-//         leftArr.addEventListener("click",()=>{
-//           if(numbers.innerHTML='06'){
-//             sectionNo5()
-//           }
-//           leftArr.addEventListener("click",()=>{
-//             if(numbers.innerHTML='07'){
-//               sectionNo6()
-//             }
-//             leftArr.addEventListener("click",()=>{
-//               if(numbers.innerHTML='08'){
-//                 sectionNo7()
-//               }
-//               leftArr.addEventListener("click",()=>{
-//                 if(numbers.innerHTML='09'){
-//                 sectionNo8()
-//                 }
-//               })
-//             })
-//           })
-//         })
-//       })
-//     })
-//   })
-// })
 
 
 
